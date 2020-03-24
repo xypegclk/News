820a092225ef1b38a5a639ff5734d44986563351
xypegclk <xypagclg@gmail.com>
2020-03-24 16:12:40 +0800

    Initial Commit

---
+++ News/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  News
+//
+//  Created by 薛義郎 on 2020/3/24.
+//  Copyright © 2020 薛義郎. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
